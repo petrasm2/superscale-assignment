70c7162b355e4b5ecf0ba68dd4a06c4dfcf9a232
Martin Petráš <martin.petras53@outlook.com>
2023-05-28 23:59:38 +0200

    fix zone.js polyfill for stackblitz

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// polyfills
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
